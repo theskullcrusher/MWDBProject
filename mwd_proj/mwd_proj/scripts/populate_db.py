f3762ac16f92c2d81aeea6e0f8f037a508ae69ab
Suraj Shah <ssshah22@asu.edu>
2017-09-10 22:13:38 +0000

    1

---
+++ mwd_proj/mwd_proj/scripts/populate_db.py
@@ -0,0 +1,20 @@
+from mwdb_proj.phase1.models import *
+from datetime import datetime
+import django
+from django.contrib.auth import User
+
+def populate_db():
+    
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    populate_db()
