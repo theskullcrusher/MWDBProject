b16bc54221264da93a7003097608654954a39b35
Suraj Sunil Shah <ssshah22@asu.edu>
2017-09-11 02:02:11 -0700

    Working Task 1

--- mwd_proj/mwd_proj/scripts/print_actor_vector.py
+++ mwd_proj/mwd_proj/scripts/print_actor_vector.py
@@ -10,6 +10,7 @@ from mwd_proj.utils.utils import *
 import traceback
 from django.db.models import Sum
 import operator
+import math
 
 def tf():
 	try:
@@ -32,7 +33,7 @@ def tf():
 			keys = tf_dict[actor.actorid].keys()
 			for key in keys:
 				sc = tf_dict[actor.actorid][key]
-				Task1.objects.create(actorid=actor.actorid, tag=key ,score=sc )
+				Task1.objects.create(actorid=actor.actorid, tag=key ,score=sc)
 	except:
 		traceback.print_exc()
 
@@ -41,21 +42,29 @@ def idf():
 
 def main():
 	try:
-		actorid = (sys.argv[1])
+		actorid = int(sys.argv[1])
 		model = str(sys.argv[2])
+		total = Task1.objects.filter(actorid=actorid).aggregate(Sum('score'))['score__sum']
+		records = Task1.objects.filter(actorid=actorid)
+		tf_dict = {}
+		actor = ImdbActorInfo.objects.get(actorid=actorid)
+		print "Actor Information: ID-{}; Name-{}; Gender-{}".format(actor.actorid, actor.name, actor.gender)
+		for record in records:
+			tf_dict[record.tag] = float(record.score / total)
 		if model.lower().strip() == 'tf':
-			total = Task1.objects.filter(actorid=actorid).aggregate(Sum('score'))['score__sum']
-			records = Task1.objects.filter(actorid=actorid)
-			tf_dict = {}
-			actor = ImdbActorInfo.objects.get(actorid=actorid)
-			print "Actor Information: ID-{}, Name-{}, Gender-{}".format(actor.actorid, actor.name, actor.gender)
-			for record in records:
-				tf_dict[record.tag] = float(record.score / total)
-			sorted_dict = sorted(tf_dict.items(), key=operator.itemgetter(1)).reverse()
-			print "\n Sorted tags:\n{}".format(sorted_dict)
-
+		#print tf_dict
+			sorted_dict = sorted(tf_dict.items(), key=operator.itemgetter(1), reverse=True)
+			print "Sorted TF tags:\n{}\n\n".format(sorted_dict)
 		else:
-			pass
+		#print tf*idf dict
+			D = Task1.objects.values('actorid').distinct().count()
+			keys = tf_dict.keys()
+			for tag in keys:
+				count = Task1.objects.filter(tag=tag).count()
+				idf_score = math.log10(float(D)/float(count))
+				tf_dict[tag] *= idf_score
+			sorted_dict = sorted(tf_dict.items(), key=operator.itemgetter(1), reverse=True)
+			print "Sorted TF-IDF tags:\n{}\n\n".format(sorted_dict)	
 	except Exception as e:
 		traceback.print_exc()
 
@@ -68,6 +77,6 @@ def elapsedTime(starttime):
 
 
 if __name__ == "__main__":
-	#main()
-	tf()
+	main()
+	#tf()
 	#idf()	
