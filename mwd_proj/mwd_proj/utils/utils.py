b02baafe326654b124020bed2eb96a8749cb16ab
Suraj Sunil Shah <ssshah22@asu.edu>
2017-09-10 23:45:57 -0700

    utils working

--- mwd_proj/mwd_proj/utils/utils.py
+++ mwd_proj/mwd_proj/utils/utils.py
@@ -9,38 +9,38 @@ from django.db.models import Max, Min
 from mwd_proj.phase1.models import *
 
 def normalize_tables():
-	max_val = long(MlRatings.objects.all().aggregate(Max('timestamp'))['timestamp__max'])
-	min_val = long(MlRatings.objects.all().aggregate(Min('timestamp'))['timestamp__min'])
-	#print max_val, min_val
-	range_ = max_val - min_val
-	print "MLRatings max {} min {} range {}".format(max_val, min_val, range_)
-	records = MlRatings.objects.all()
-	for record in records:
-		val = long(record.timestamp)
-		norm_val = float(val - min_val) / float(range_)
-		record.norm_weight = str(norm_val)
-		record.save()
+	# max_val = long(MlRatings.objects.all().aggregate(Max('timestamp'))['timestamp__max'])
+	# min_val = long(MlRatings.objects.all().aggregate(Min('timestamp'))['timestamp__min'])
+	# #print max_val, min_val
+	# range_ = max_val - min_val
+	# print "MLRatings max {} min {} range {}".format(max_val, min_val, range_)
+	# records = MlRatings.objects.all()
+	# for record in records:
+	# 	val = long(record.timestamp)
+	# 	norm_val = float(val - min_val) / float(range_)
+	# 	record.norm_weight = str(norm_val)
+	# 	record.save()
 
-        max_val = long(MlTags.objects.all().aggregate(Max('timestamp'))['timestamp__max'])
-        min_val = long(MlTags.objects.all().aggregate(Min('timestamp'))['timestamp__min'])
-        #print max_val, min_val
-        range_ = max_val - min_val
-	print "MLTags max {} min {} range {}".format(max_val, min_val, range_)
-        records = MlTags.objects.all()
-        for record in records:
-                val = long(record.timestamp)
-                norm_val = float(val - min_val) / float(range_)
-                record.norm_weight = str(norm_val)
-                record.save()
+ #        max_val = long(MlTags.objects.all().aggregate(Max('timestamp'))['timestamp__max'])
+ #        min_val = long(MlTags.objects.all().aggregate(Min('timestamp'))['timestamp__min'])
+ #        #print max_val, min_val
+ #        range_ = max_val - min_val
+	# print "MLTags max {} min {} range {}".format(max_val, min_val, range_)
+ #        records = MlTags.objects.all()
+ #        for record in records:
+ #                val = long(record.timestamp)
+ #                norm_val = float(val - min_val) / float(range_)
+ #                record.norm_weight = str(norm_val)
+ #                record.save()
 
-        max_val = long(MovieActor.objects.all().aggregate(Max('movie_actor_rank'))['movie_actor_rank__max'])
-        min_val = long(MovieActor.objects.all().aggregate(Min('movie_actor_rank'))['movie_actor_rank__min'])
+        max_val = long(MovieActor.objects.all().aggregate(Max('actor_movie_rank'))['actor_movie_rank__max'])
+        min_val = long(MovieActor.objects.all().aggregate(Min('actor_movie_rank'))['actor_movie_rank__min'])
         #print max_val, min_val
         range_ = max_val - min_val
         print "MovieActor max {} min {} range {}".format(max_val, min_val, range_)
         records = MovieActor.objects.all()
         for record in records:
-                val = long(record.movie_actor_rank)
+                val = long(record.actor_movie_rank)
                 norm_val = float(val - min_val) / float(range_)
                 record.norm_rank = str(norm_val)
                 record.save()
